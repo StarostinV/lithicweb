ecb877cf3f026acd928d9989fecae34dd10828d0
StarostinV <vladimir.starostin@uni-tuebingen.de>
2024-06-29 01:45:38 +0200

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,44 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: './src/main.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    devServer: {
-        static: path.resolve(__dirname, 'dist'),
-        port: 8000,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            filename: 'index.html',
-            scriptLoading: 'defer',
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production' || !argv.mode;
+
+    return {
+        entry: './src/main.js',
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+        },
+        devServer: {
+            static: path.resolve(__dirname, 'dist'),
+            port: 8000,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './index.html',
+                filename: 'index.html',
+                scriptLoading: 'defer',
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
                     }
                 }
-            }
-        ]
-    },
-    mode: 'development'
+            ]
+        },
+        optimization: isProduction ? {
+            minimize: true,
+        } : {},
+        mode: isProduction ? 'production' : 'development'
+    };
 };
